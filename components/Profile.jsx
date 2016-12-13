1c25ba20b73e1896178af563747fc4237ce2ff82
Junya Nakazato <nakazato_junya@ca-adv.co.jp>
2016-12-13 14:58:22 +0900

    profile記載

--- components/Profile.jsx
+++ components/Profile.jsx
@@ -1,14 +1,31 @@
 import React from 'react';
-
 import Header from 'grommet/components/Header';
 import Footer from 'grommet/components/Footer';
 import Heading from 'grommet/components/Heading';
+import Headline from 'grommet/components/Headline';
 import Hero from 'grommet/components/Hero';
 import Paragraph from 'grommet/components/Paragraph';
 import Box from 'grommet/components/Box';
-import Card from 'grommet/components/Card';
-import Anchor from 'grommet/components/Anchor';
+import Markdown from 'grommet/components/Markdown';
 
+const backgroundImage = '../src/hero-background.jpg';
+
+const profile = `
+\`\`\`json
+{
+  "name": "Junya Nakazato",
+  "job": "Front-End and Back-End Web Developer",
+  "live": "Okinawa, Japan",
+  "love": [ "League of Legends", "GUILTY GEAR XX ΛCORE" ],
+  "frontend": [ "JavaScript", "React", "Material-UI", "jQuery" ],
+  "backend": [ "Node", "Ruby", "Java", "PHP", "Objective-C",
+    "Rails", "Spring Framework", "FuelPHP" ],
+  "infrastructure": [ "AWS", "ECS", "CloudFront", "Docker", "Chef" ],
+  "ops": [ "NewRelic", "Datadog", "Sentry" ],
+  "tools": [ "Mac", "Vim", "zsh", "Vimperator" , "XCode" ]
+}
+\`\`\`
+`;
 
 const contents = () => (
   <content>
@@ -22,14 +39,11 @@ const contents = () => (
       />
     </Header>
 
-    <Hero backgroundImage="http://i.imgur.com/spjWbAU.jpg">
-      <Box colorIndex="grey-2-a">
-        <Card
-          label="Profile"
-          heading="Junya Nakazato"
-          description="My Skill: <br />JavaScript, React, Rails, PHP, Docker, AWS, ECS, Vim"
-          link={<Anchor href="#" primary label="Link" />}
-        />
+    <Hero backgroundImage={backgroundImage}>
+      <Box colorIndex="grey-1-a">
+        <br />
+        <Headline size="small">Profile</Headline>
+        <Markdown content={profile} />
       </Box>
     </Hero>
 
