32540ba5ec9bb6ba96aecebd91ba0693ea461fc2
Junya Nakazato <nakazato_junya@ca-adv.co.jp>
2016-12-13 12:39:07 +0900

    cssをstyle-loaderへ

--- containers/App.jsx
+++ containers/App.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
-
 import 'grommet/grommet.min.css';
 import 'grommet/scss/vanilla/index.scss';
-
 import App from 'grommet/components/App';
+
+import '../src/main.css';
 import Profile from '../components/Profile';
 
 const app = () => (
