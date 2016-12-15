10d4defe867a8e00481d8c8798a00f7fbdf3d654
Junya Nakazato <nakazato_junya@ca-adv.co.jp>
2016-12-15 10:47:57 +0900

    uglify設定

--- webpack.config.prod.babel.js
+++ webpack.config.prod.babel.js
@@ -5,7 +5,12 @@ module.exports = {
   devtool: false,
   plugins: [
     new webpack.optimize.DedupePlugin(),
-    new webpack.optimize.UglifyJsPlugin(),
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false,
+      },
+      comments: false,
+    }),
     new webpack.optimize.AggressiveMergingPlugin(),
   ],
 };
