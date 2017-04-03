63458004f13a72fdf4b75b1653272283179d5832
Junya Nakazato <nakazato_junya@ca-adv.co.jp>
2017-04-03 10:47:19 +0900

    optimized prod. update pkg.

--- webpack.config.prod.babel.js
+++ webpack.config.prod.babel.js
@@ -3,15 +3,6 @@ import webpack from 'webpack';
 module.exports = {
   devtool: false,
   plugins: [
-    new webpack.optimize.UglifyJsPlugin({
-      compress: {
-        warnings: false,
-      },
-      comments: false,
-    }),
-    new webpack.LoaderOptionsPlugin({
-      minimize: true,
-    }),
     new webpack.optimize.AggressiveMergingPlugin(),
   ],
 };
