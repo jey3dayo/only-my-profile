337270c849c19c4e61b6fb8ba362e24a00884d5a
Junya Nakazato <nakazato_junya@ca-adv.co.jp>
2021-02-25 13:21:07 +0900

    eslint, babel再設定

--- containers/Template.jsx
+++ containers/Template.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Helmet from 'react-helmet';
 
-const Template = (props) => (
+const Template = props => (
   <div>
     <Helmet defaultTitle="jey3dayo.net" />
     {props.children}
